ef0c75ebacd674d7ca8a5fbf43ed231821d8645a
Nitin Choudhary <nitin@blotout.io>
2021-09-02 22:40:52 +0530

    Added Blotout SDK iOS native calls

--- ios/RNBlotOutSDKModule.swift
+++ ios/RNBlotOutSDKModule.swift
@@ -6,22 +6,25 @@
 //
 
 import Foundation;
+import BlotoutAnalyticsSDK
 
 @objc(RNBlotOutSDKModule)
 class RNBlotOutSDKModule: NSObject {
-  @objc
-  func constantsToExport() -> [AnyHashable : Any]! {
-    return ["count": 1]
-  }
-
-  @objc
-  static func requiresMainQueueSetup() -> Bool {
-    return true
-  }
-    
     @objc
     func initializeAnalyticsEngine(_ blotoutSDKKey: String, endPointUrl: String){
-        
+        let boaSDK : BlotoutAnalytics
+            boaSDK =  BlotoutAnalytics.sharedInstance()!
+            let config = BlotoutAnalyticsConfiguration.init(token: blotoutSDKKey, withUrl: endPointUrl)
+            //config.launchOptions = launchOptions;
+            boaSDK.`init`(config) { (isSuccess : Bool, errorObj:Error?) in
+                if isSuccess{
+                    print("Integration Successful.")
+                    boaSDK.capture("AppLaunchedWithBOSDK", withInformation: nil)
+                }else{
+                    print("Unexpected error:.")
+                }
+                boaSDK.capture("AppLaunchedWithBOSDK", withInformation: nil)
+            }
     }
     
     @objc
@@ -30,16 +33,24 @@ class RNBlotOutSDKModule: NSObject {
     }
     
     @objc
-    func capturePersonal(_ eventName: String, eventInfo: NSDictionary, isPHI: CFBoolean){
+    func capturePersonal(_ eventName: String, eventInfo: NSDictionary, isPHI: Bool){
+        let boaSDK : BlotoutAnalytics
+        boaSDK =  BlotoutAnalytics.sharedInstance()!
+        boaSDK.capturePersonal(eventName , withInformation: eventInfo as! [AnyHashable : Any], isPHI: isPHI )
         
     }
     
     @objc
     func capture(_ eventName: String, eventInfo: NSDictionary){
+        let boaSDK : BlotoutAnalytics
+        boaSDK =  BlotoutAnalytics.sharedInstance()!
+        boaSDK.capture(eventName , withInformation: eventInfo as! [AnyHashable : Any])
     }
     
     @objc
-    func mapID(_ mapIdData: String, withInformation:NSDictionary){
-        
+    func mapID(_ mapIdData: BOAMapIDDataModel, withInformation:NSDictionary){
+        let boaSDK : BlotoutAnalytics
+        boaSDK =  BlotoutAnalytics.sharedInstance()!
+        boaSDK.mapID(mapIdData, withInformation: <#T##[AnyHashable : Any]?#>)
     }
 }
