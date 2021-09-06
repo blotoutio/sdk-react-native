dd9505164ffd321a0f2aea94233da2f77af30c28
NejcZdovc <nejc.zdovc@3zsistemi.si>
2021-09-06 11:16:55 +0200

    Setup
    Resolves #3

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,12 @@
 module.exports = {
+  root: true,
   extends: ['@react-native-community', 'plugin:prettier/recommended'],
   plugins: ['simple-import-sort'],
-  root: true,
   rules: {
     'import/order': 'off',
     'simple-import-sort/exports': 'error',
     'simple-import-sort/imports': 'error',
     'sort-imports': 'off',
   },
+  ignorePatterns: ['/lib', '/android', '/ios', '/node_modules'],
 }
