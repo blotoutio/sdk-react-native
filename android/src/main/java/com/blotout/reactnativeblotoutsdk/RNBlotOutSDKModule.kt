11e80d32c4ba23e156f5e7c902a4fb0d7e95b750
Nitin Choudhary <nitin@blotout.io>
2022-01-23 16:57:15 +0530

    Upgrade to 0.10.0 version

--- android/src/main/java/com/blotout/reactnativeblotoutsdk/RNBlotOutSDKModule.kt
+++ android/src/main/java/com/blotout/reactnativeblotoutsdk/RNBlotOutSDKModule.kt
@@ -5,6 +5,10 @@ import android.widget.Toast
 import com.analytics.blotout.BlotoutAnalytics
 import com.analytics.blotout.BlotoutAnalyticsConfiguration
 import com.analytics.blotout.model.MapIDData
+import com.analytics.blotout.model.CompletionHandler
+import com.analytics.blotout.model.TransactionData
+import com.analytics.blotout.model.Item
+import com.analytics.blotout.model.Persona
 import com.facebook.react.bridge.*
 
 
@@ -24,12 +28,19 @@ class RNBlotOutSDKModule(private val reactContext: ReactApplicationContext) : Re
      * @param endPointUrl End point Url  as String
      */
     @ReactMethod
-    fun init(blotoutSDKKey: String, endPointUrl: String) {
+    fun init(blotoutSDKKey: String, endPointUrl: String, callBack: Callback ) {
         ///Initialize SDK here
         val blotoutAnalyticsConfiguration = BlotoutAnalyticsConfiguration()
         blotoutAnalyticsConfiguration.blotoutSDKKey = blotoutSDKKey
         blotoutAnalyticsConfiguration.endPointUrl = endPointUrl
-        BlotoutAnalytics.init(reactContext.applicationContext as Application, blotoutAnalyticsConfiguration)
+        BlotoutAnalytics.init(reactContext.applicationContext as Application, blotoutAnalyticsConfiguration, object : CompletionHandler {
+            override fun onError(code: Int, msg: String) {
+                callBack.invoke(code,msg);
+            }
+            override fun onSuccess() {
+                callBack.invoke();
+            }
+        })
     }
 
     /**
@@ -85,6 +96,79 @@ class RNBlotOutSDKModule(private val reactContext: ReactApplicationContext) : Re
     fun getUserId(userIDCallback : Callback) {
         try {
             userIDCallback.invoke(BlotoutAnalytics.getUserId())
+        }catch (e:Exception){
+            Toast.makeText(reactContext.applicationContext,e.message!!,Toast.LENGTH_LONG).show()
+        }
+    }
+
+    @ReactMethod
+    fun transaction(options: ReadableMap,withInformation: ReadableMap) {
+        try {
+
+            val transaction_id =  options.getString("transaction_id")
+            val transaction_currency =  options.getString("transaction_currency")
+            val transaction_payment =  options.getString("transaction_payment")
+            val transaction_total =  options.getDouble("transaction_total")
+            val transaction_discount =  options.getInt("transaction_discount")
+            val transaction_shipping =  options.getInt("transaction_shipping")
+            val transaction_tax =  options.getDouble("transaction_tax")
+
+            var transactionData = TransactionData(transaction_id!!,transaction_currency,transaction_payment,
+                                                   transaction_total,transaction_discount,transaction_shipping, transaction_tax)
+
+            BlotoutAnalytics.transaction(transactionData, withInformation.toHashMap())
+        }catch (e:Exception){
+            Toast.makeText(reactContext.applicationContext,e.message!!,Toast.LENGTH_LONG).show()
+        }
+    }
+
+    @ReactMethod
+    fun item(options: ReadableMap,withInformation: ReadableMap) {
+        try {
+            val item_id =  options.getString("item_id")
+            val item_name =  options.getString("item_name")
+            val item_sku =  options.getString("item_sku")
+            val item_category =  null//options.getArray("item_category")
+            val item_price =  options.getInt("item_price")
+            val item_currency =  options.getString("item_currency")
+            val item_quantity =  options.getInt("item_quantity")
+            var itemData = Item(item_id!!,item_name,item_sku,
+                                item_category,item_price,item_currency, item_quantity)
+
+            BlotoutAnalytics.item(itemData, withInformation.toHashMap())
+        }catch (e:Exception){
+            Toast.makeText(reactContext.applicationContext,e.message!!,Toast.LENGTH_LONG).show()
+        }
+    }
+
+    @ReactMethod
+    fun persona(options: ReadableMap,withInformation: ReadableMap) {
+        try {
+
+            val persona_id =  options.getString("persona_id")
+            val persona_firstname =  options.getString("persona_firstname")
+            val persona_lastname =  options.getString("persona_lastname")
+            val persona_middlename =  options.getString("persona_middlename")
+            val persona_username =  options.getString("persona_username")
+            val persona_dob =  options.getString("persona_dob")
+            val persona_email =  options.getString("persona_email")
+
+            val persona_number =  options.getString("persona_number")
+            val persona_address =  options.getString("persona_address")
+            val persona_city =  options.getString("persona_city")
+            val persona_state =  options.getString("persona_state")
+            val persona_zip =  options.getInt("persona_zip")
+
+            val persona_country =  options.getString("persona_country")
+            val persona_gender =  options.getString("persona_gender")
+            val persona_age =  options.getInt("persona_age")
+
+            var personaData = Persona(persona_id!!,persona_firstname,persona_lastname,
+                                persona_middlename,persona_username,persona_dob, persona_email,
+                               persona_number,persona_address, persona_city,persona_state,
+                               persona_zip,persona_country,persona_gender,persona_age)
+
+            BlotoutAnalytics.persona(personaData, withInformation.toHashMap())
         }catch (e:Exception){
             Toast.makeText(reactContext.applicationContext,e.message!!,Toast.LENGTH_LONG).show()
         }
--- example/src/app/index.tsx
+++ example/src/app/index.tsx
@@ -6,7 +6,10 @@ const App = () => {
     console.log(RNBlotOutSDKModule)
     RNBlotOutSDKModule.init(
       'Y4BFUDCNNZQZAUE',
-      'https://sandbox.blotout.io/sdk/'
+      'https://sandbox.blotout.io/sdk/',
+      (errorCode: any) => {
+        console.log(`Created a new event with id ${errorCode}`)
+      }
     )
     const withInformation = new Map()
     withInformation.set('Platform', 'ReactNative')
@@ -17,12 +20,31 @@ const App = () => {
       { emailId: 'developers@blotout.io', bloodType: 'A+' },
       true
     )
-
+    ////---------Get User ID---------
     RNBlotOutSDKModule.getUserId((userid: any) => console.log(userid))
-
+    ////---------MAP ID API---------
     var externalID = '92j2jr230r-232j9j2342j3-jiji'
     var provider = 'sass'
-    RNBlotOutSDKModule.mapID(externalID, provider, null)
+    RNBlotOutSDKModule.mapID(externalID, provider, withInformation)
+    //---------Transaction API---------
+    const transactionData = new Map()
+    transactionData.set('transaction_id', '12345')
+    transactionData.set('transaction_currency', 'INR')
+    transactionData.set('transaction_payment', '123')
+    transactionData.set('transaction_total', 12345.65)
+    RNBlotOutSDKModule.transaction(transactionData, null)
+    //---------Item API---------
+    const itemData = new Map()
+    itemData.set('item_id', '12345')
+    itemData.set('item_name', 'Toy')
+    itemData.set('item_sku', '12')
+    itemData.set('item_currency', 'INR')
+    RNBlotOutSDKModule.item(itemData, null)
+    ////---------Persona API---------
+    const personaData = new Map()
+    personaData.set('persona_id', '12345')
+    personaData.set('persona_firstname', 'XYZ CHG')
+    RNBlotOutSDKModule.persona(personaData, null)
   })
 
   return null
