2c60897fffe2d71293405393567c8be0e9cbfb9b
Poonam Tiwari <poonamt3489@gmail.com>
2022-01-28 21:03:31 +0530

    Linting error: spaces removed

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,12 @@
 import { NativeModules } from 'react-native'
 
 export namespace BlotoutSDK {
-
-    export const init = NativeModules.RNBlotOutSDKModule.initializeAnalyticsEngine
-    export const capture = NativeModules.RNBlotOutSDKModule.capture
-    export const capturePersonal = NativeModules.RNBlotOutSDKModule.capturePersonal
-    export const getUserId = NativeModules.RNBlotOutSDKModule.getUserId
-    export const mapId = NativeModules.RNBlotOutSDKModule.mapId
-    export const transaction = NativeModules.RNBlotOutSDKModule.transaction
-    export const item = NativeModules.RNBlotOutSDKModule.item
-    export const persona = NativeModules.RNBlotOutSDKModule.persona
-    
+  export const init = NativeModules.RNBlotOutSDKModule.initializeAnalyticsEngine
+  export const capture = NativeModules.RNBlotOutSDKModule.capture
+  export const capturePersonal = NativeModules.RNBlotOutSDKModule.capturePersonal
+  export const getUserId = NativeModules.RNBlotOutSDKModule.getUserId
+  export const mapId = NativeModules.RNBlotOutSDKModule.mapId
+  export const transaction = NativeModules.RNBlotOutSDKModule.transaction
+  export const item = NativeModules.RNBlotOutSDKModule.item
+  export const persona = NativeModules.RNBlotOutSDKModule.persona 
 }
