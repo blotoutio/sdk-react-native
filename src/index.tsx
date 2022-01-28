88269de679127576b07656816ad0bbbebd49f703
Poonam Tiwari <poonamt3489@gmail.com>
2022-01-28 20:53:50 +0530

    Trial for NS & version bump

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,14 @@
 import { NativeModules } from 'react-native'
 
-export default NativeModules.RNBlotOutSDKModule
+export namespace BlotoutSDK {
+
+    export const init = NativeModules.RNBlotOutSDKModule.initializeAnalyticsEngine
+    export const capture = NativeModules.RNBlotOutSDKModule.capture
+    export const capturePersonal = NativeModules.RNBlotOutSDKModule.capturePersonal
+    export const getUserId = NativeModules.RNBlotOutSDKModule.getUserId
+    export const mapId = NativeModules.RNBlotOutSDKModule.mapId
+    export const transaction = NativeModules.RNBlotOutSDKModule.transaction
+    export const item = NativeModules.RNBlotOutSDKModule.item
+    export const persona = NativeModules.RNBlotOutSDKModule.persona
+    
+}
