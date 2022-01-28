6c17ba469ad7628c8b456c4019c5d40b0d440874
Poonam Tiwari <poonamt3489@gmail.com>
2022-01-28 21:27:18 +0530

    Lint Fix

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,8 @@ import { NativeModules } from 'react-native'
 export namespace BlotoutSDK {
   export const init = NativeModules.RNBlotOutSDKModule.initializeAnalyticsEngine
   export const capture = NativeModules.RNBlotOutSDKModule.capture
-  export const capturePersonal = NativeModules.RNBlotOutSDKModule.capturePersonal
+  export const capturePersonal =
+    NativeModules.RNBlotOutSDKModule.capturePersonal
   export const getUserId = NativeModules.RNBlotOutSDKModule.getUserId
   export const mapId = NativeModules.RNBlotOutSDKModule.mapId
   export const transaction = NativeModules.RNBlotOutSDKModule.transaction
